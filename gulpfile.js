5240ef298779865d97e020c3304cbd9ab4fabda9
David Avellaneda <g.avella@gmail.com>
2015-10-15 00:01:09 -0500

    adding descriptions and agenda intro

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cssMin = require('gulp-cssmin'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create()
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat')
 
 gulp.task('dist-sass', function(cb){
   return gulp.src('content/sass/**/*.sass')
@@ -16,22 +18,30 @@ gulp.task('dist-sass', function(cb){
   .pipe(browserSync.stream())
 })
 
+gulp.task('dist-js', function () {
+  return gulp.src('content/js/**/*.js')
+            .pipe(concat('main.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('public/js'))
+})
+
 gulp.task('watch', function (cb) {
   gulp.watch('content/sass/**/*.sass', ['dist-sass'])
+  gulp.watch('content/js/**/*.js', ['dist-js'])
   gulp.watch('public/*.html').on('change', browserSync.reload)
 })
 
 gulp.task('browser-sync', function(cb) {
   browserSync.init({
     server: {
-      baseDir: './',
-      proxy: 'localhost:6000'
+      baseDir: './public'
     }
   })
 })
 
-gulp.task('dist', ['dist-sass'], function(){
+gulp.task('dist', ['dist-sass', 'dist-js'], function(){
   console.log('dist ready')
 })
 
-gulp.task('default', ['browser-sync', 'dist', 'watch'] , function () {})
+gulp.task('default', ['browser-sync', 'dist', 'watch'] , function () {
+})
